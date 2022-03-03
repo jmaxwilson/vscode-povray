be2df07e44f6ac8c92f75b9900ff0b492037c061
Jeremy A. Cunningham <jac@kennebel.com>
2022-03-02 21:12:56 -0700

    Final prep to finish Issue #3

--- out/extension.js
+++ out/extension.js
@@ -465,19 +465,4 @@ function getDirName(filepath, context) {
     return dirname;
 }
 exports.getDirName = getDirName;
-// Removed from package.json
-/*
-                "povray.docker.image": {
-                    "type": "string",
-                    "default": "jmaxwilson/povray",
-                    "markdownDescription": "Specify the docker image that will be used to run povray through docker. Compatible images available from https://hub.docker.com/r/jmaxwilson/povray "
-                },
-
-                "povray.docker.enableDocker": {
-                    "type": "boolean",
-                    "default": false,
-                    "markdownDescription": "Use [Docker](https://www.docker.com/products/docker-desktop) to run POV-Ray."
-                },
-
-*/ 
 //# sourceMappingURL=extension.js.map
